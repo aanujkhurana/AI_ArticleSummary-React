a132c71cf0bb716e2320eba64078fc8f2ca3a6ae
aanujkhurana <aanujkhurana@gmail.com>
2024-02-21 11:20:38 +1000

    added url history and summarytext

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -12,15 +12,29 @@ const Demo = () => {
         }
     )
 
+    useEffect(() => {
+        const articlesFromLocalStorage = JSON.parse(localStorage.getItem('articles'));
+        if (articlesFromLocalStorage) {
+            setAllArticle(articlesFromLocalStorage);
+        }
+    }, []);
+
+    const [allArticle, setAllArticle] = useState([]);
+
     const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
 
     const handleSubmit = async (e) => {
+        e.preventDefault();
         const { data } = await getSummary({ articleUrl: article.url });
-
         if (data?.summary) {
             const newArticle = { ...article, summary: data.summary };
+            const updatedAllArticles = [...allArticle, newArticle];
+
             setArticle(newArticle);
+            setAllArticle(updatedAllArticles);
+
             console.log(newArticle);
+            localStorage.setItem('articles', JSON.stringify(updatedAllArticles));
         }
     }
 
@@ -48,9 +62,58 @@ const Demo = () => {
                     </button>
                 </form>
                 {/* BROSWER URL HISTORY */}
-                <div></div>
+                <div
+                    className='flex flex-col gap-1 max-h-60 overflow-y-auto'>
+                    {allArticle.map((item, index) => (
+                        <div
+                            key={`link-${index}`}
+                            onClick={() => setArticle(item)}
+                            className='link_card'>
+                            <div className='copy_btn'>
+                                <img
+                                    src={copy}
+                                    alt='copyIcon'
+                                    className='w-[40%] h-[40%] object-contain'
+                                />
+                            </div>
+                            <p className='flex-1 font-satoshi text-blue-700 font-medium text-sm truncate'>
+                                {item.url}
+                            </p>
+                        </div>
+                    ))}
+                </div>
             </div>
             {/* RESULT */}
+            <div className='my-10 max-w-full flex justify-center items-center'>
+                {isFetching ? (
+                    <img
+                        src={loader}
+                        alt='loading icon'
+                        className='w-20 h-20 object-contain' />
+                ) : error ? (
+                    <p className='font-inter font-bold text-black text-center'>
+                        Well, That wasn't suppose to happen
+                        <br />
+                        <span className='font-satoshi font-normal text-gray-700'>
+                            {error?.data?.error}
+                        </span>
+                    </p>
+                ) : (
+                    article.summary && (
+                        <div className='flex flex-col gap-3'>
+                            <h2 className='font-satoshi font-bold text-gray-600 text-xl'>
+                                Article <span className='blue_gradient'>Summary</span>
+                            </h2>
+                            <div className='summary_box'>
+                                <p className='font-inter font-medium text-sm text-gray-700'>
+                                    {article.summary}
+                                </p>
+                            </div>
+                        </div>
+                    )
+                )
+                }
+            </div>
         </section>
     )
 }
